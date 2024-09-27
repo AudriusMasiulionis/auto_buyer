2fd4fd8c5459a7f0b3163bfc4c9b595a8fae5d81
Audrius <audrius.masiulionis@gmail.com>
2024-09-27 21:51:35 +0300

    added missing files

---
+++ apps/api/Api/Endpoints/ContractPutEndpoint.cs
@@ -0,0 +1,23 @@
+using FastEndpoints;
+
+namespace Api.Endpoints;
+
+public class ContractPutEndpoint : Endpoint<MyRequest, MyResponse>
+{
+    public override void Configure()
+    {
+        Post("/api/user/create");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(MyRequest req, CancellationToken ct)
+    {
+        var obj = new MyResponse
+        {
+            FullName = req.FirstName + " " + req.LastName,
+            IsOver18 = req.Age > 18
+        };
+
+        await SendAsync(obj, cancellation: ct);
+    }
+}
